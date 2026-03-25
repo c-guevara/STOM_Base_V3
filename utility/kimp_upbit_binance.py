dbf1a44dc1cac5063972019acf482128478db0c0
youse <youseonho@gmail.com>
2026-03-25 17:11:33 +0900

    김프창 하나의 프로세스로 작동되도록 변경

--- utility/kimp_upbit_binance.py
+++ utility/kimp_upbit_binance.py
@@ -1,7 +1,7 @@
 
 import re
+import sys
 import json
-import time
 import uuid
 import asyncio
 import pyupbit
@@ -9,58 +9,61 @@ import requests
 import websockets
 import pandas as pd
 from bs4 import BeautifulSoup
-from multiprocessing import Process
-from binance import AsyncClient, BinanceSocketManager
+from PyQt5.QtWidgets import QApplication
 from utility.setting_base import ui_num, columns_kp
-from utility.static import comma2float, threading_timer
+from PyQt5.QtCore import QThread, pyqtSignal, QTimer
+from binance import AsyncClient, BinanceSocketManager
+from utility.static import comma2float, thread_decorator
 
 
 class Kimp:
     def __init__(self, qlist):
         """
-        windowQ, soundQ, queryQ, teleQ, chartQ, hogaQ, webcQ, backQ, creceivQ, ctraderQ,  cstgQ, liveQ, kimpQ, wdzservQ, totalQ
-           0        1       2      3       4      5      6      7       8         9         10     11    12      13       14
+        windowQ, soundQ, queryQ, teleQ, chartQ, hogaQ, webcQ, backQ, creceivQ, ctraderQ,  cstgQ, liveQ, wdzservQ
+           0        1       2      3       4      5      6      7       8         9         10     11      12
         """
+        app = QApplication(sys.argv)
+
         self.windowQ   = qlist[0]
-        self.kimpQ     = qlist[12]
         self.usdtokrw  = None
-        self.proc_webs = None
-        self.codes     = None
-        self.threadrun = True
+        self.thread_ws = None
+        self.codes     = pyupbit.get_tickers(fiat="KRW")
         self.df        = pd.DataFrame(columns=columns_kp)
-        self.Start()
 
-    def Start(self):
-        self.codes = pyupbit.get_tickers(fiat="KRW")
         self.ConvertedCurrency()
-        self.WebsSocketsStart(self.kimpQ)
-        while True:
-            data = self.kimpQ.get()
-            if data[0] == 'upbit':
-                data = data[1]
-                code = data['code'].replace('KRW-', '')
-                c    = data['trade_price']
-                self.df.loc[code, ['종목명', '업비트(원)']] = [code, c]
-            elif data[0] == 'binance' and self.usdtokrw is not None:
-                data = data[1]
-                for x in data:
-                    if re.search('USDT$', x['s']) is not None:
-                        code = x['s'].replace('USDT', '')
-                        c    = float(x['c'])
-                        self.df.loc[code, '바이낸스(달러)'] = c
 
-                self.df['대비(원)'] = self.df['업비트(원)'] - self.df['바이낸스(달러)'] * self.usdtokrw
-                self.df['대비율(%)'] = self.df['대비(원)'] / self.df['업비트(원)'] * 100
-                self.df.dropna(inplace=True)
-                self.df.sort_values(by=['대비율(%)'], ascending=False, inplace=True)
-                self.windowQ.put((ui_num['김프'], self.df, self.usdtokrw))
-            elif data == '프로세스종료':
-                self.threadrun = False
-                self.WebProcessKill()
-                break
+        self.qtimer = QTimer()
+        self.qtimer.setInterval(60 * 1000)
+        self.qtimer.timeout.connect(self.ConvertedCurrency)
+        self.qtimer.start()
 
-        time.sleep(3)
+        self.thread_ws = WebSocketManager(self.codes)
+        self.thread_ws.signal1.connect(self.UpdateUpbitData)
+        self.thread_ws.signal2.connect(self.UpdateBinanceData)
+        self.thread_ws.start()
 
+        app.exec_()
+
+    def UpdateUpbitData(self, data):
+        code = data['code'].replace('KRW-', '')
+        c = data['trade_price']
+        self.df.loc[code, ['종목명', '업비트(원)']] = [code, c]
+
+    def UpdateBinanceData(self, data):
+        for x in data:
+            if re.search('USDT$', x['s']) is not None:
+                code = x['s'].replace('USDT', '')
+                c = float(x['c'])
+                self.df.loc[code, '바이낸스(달러)'] = c
+
+        if self.usdtokrw is not None:
+            self.df['대비(원)'] = self.df['업비트(원)'] - self.df['바이낸스(달러)'] * self.usdtokrw
+            self.df['대비율(%)'] = self.df['대비(원)'] / self.df['업비트(원)'] * 100
+            self.df.dropna(inplace=True)
+            self.df.sort_values(by=['대비율(%)'], ascending=False, inplace=True)
+            self.windowQ.put((ui_num['김프'], self.df, self.usdtokrw))
+
+    @thread_decorator
     def ConvertedCurrency(self):
         try:
             html = requests.get('https://finance.naver.com/marketindex/exchangeDetail.naver?marketindexCd=FX_USDKRW').text
@@ -70,30 +73,29 @@ class Kimp:
         except:
             pass
 
-        if self.threadrun:
-            threading_timer(60, self.ConvertedCurrency)
 
-    def WebsSocketsStart(self, q):
-        self.proc_webs = Process(target=WebSocketManager, args=(self.codes, q), daemon=True)
-        self.proc_webs.start()
+class WebSocketManager(QThread):
+    signal1 = pyqtSignal(object)
+    signal2 = pyqtSignal(object)
 
-    def WebProcessKill(self):
-        if self.proc_webs is not None and self.proc_webs.is_alive(): self.proc_webs.kill()
-
-
-class WebSocketManager:
-    def __init__(self, codes, q):
+    def __init__(self, codes):
+        super().__init__()
         self.codes       = codes
-        self.q           = q
+        self.loop        = None
         self.wsk_upbit   = None
         self.wsk_binance = None
         self.con_upbit   = False
         self.con_binance = False
 
-        loop = asyncio.get_event_loop()
-        asyncio.ensure_future(self.run_upbit())
-        asyncio.ensure_future(self.run_binance())
-        loop.run_forever()
+    def run(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_until_complete(self._run())
+
+    async def _run(self):
+        upbit_task = asyncio.create_task(self.run_upbit())
+        binance_task = asyncio.create_task(self.run_binance())
+        await asyncio.gather(upbit_task, binance_task)
 
     async def run_upbit(self):
         await self.connect_upbit()
@@ -129,7 +131,7 @@ class WebSocketManager:
             try:
                 data = await self.wsk_upbit.recv()
                 data = json.loads(data)
-                self.q.put(('upbit', data))
+                self.signal1.emit(data)
             except:
                 await self.disconnect_ticker()
 
@@ -139,7 +141,7 @@ class WebSocketManager:
             while self.con_binance:
                 try:
                     data = await self.wsk_binance.recv()
-                    self.q.put(('binance', data))
+                    self.signal2.emit(data)
                 except:
                     self.con_binance = False
 
