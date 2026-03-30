053cdb7701260efefd2a2a709be8d073fb339274
youse <youseonho@gmail.com>
2026-03-30 17:03:32 +0900

    텔레그램봇 타임아웃 5초 -> 30초로 변경 및 예외처리 강화

--- utility/telegram_bot.py
+++ utility/telegram_bot.py
@@ -5,6 +5,7 @@ import pandas as pd
 from threading import Thread
 from traceback import format_exc
 from PyQt5.QtCore import QThread
+from telegram.request import HTTPXRequest
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 from utility.setting_base import ui_num
@@ -17,23 +18,25 @@ class TelegramBot(QThread):
            0        1       2      3       4      5      6      7       8         9         10     11      12
         """
         super().__init__()
-        self.windowQ     = qlist[0]
-        self.teleQ       = qlist[3]
-        self.ctraderQ    = qlist[9]
-        self.cstgQ       = qlist[10]
-        self.wdzservQ    = qlist[12]
-        self.dict_set    = dict_set
+        self.windowQ       = qlist[0]
+        self.teleQ         = qlist[3]
+        self.ctraderQ      = qlist[9]
+        self.cstgQ         = qlist[10]
+        self.wdzservQ      = qlist[12]
+        self.dict_set      = dict_set
 
-        self.token       = None
-        self.chat_id     = None
-        self.application = None
-        self.running     = False
+        self.token         = None
+        self.chat_id       = None
+        self.loop          = None
+        self.application   = None
+        self.message_queue = None
+        self.running       = False
 
+    def run(self):
         self.message_queue = asyncio.Queue()
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
 
-    def run(self):
         Thread(target=self.moniter_queue, daemon=True).start()
         self.loop.create_task(self.process_messages())
 
@@ -42,10 +45,18 @@ class TelegramBot(QThread):
         self.chat_id = self.dict_set[f'텔레그램사용자아이디{gubun}']
 
         if self.token and self.chat_id:
+            request = HTTPXRequest(
+                connection_pool_size=8,
+                read_timeout=30,
+                write_timeout=30,
+                connect_timeout=30,
+            )
+
             self.running = True
             self.application = (
                 ApplicationBuilder()
                 .token(self.token)
+                .request(request)
                 .post_init(self.setup_application)
                 .build()
             )
@@ -61,12 +72,13 @@ class TelegramBot(QThread):
             update_queue = await self.application.updater.start_polling()
 
             keyboard = [
-                ['주식      체결목록', '주식      거래목록', '주식      잔고평가', '주식      잔고청산', '주식      전략중지'],
-                ['해선      체결목록', '해선      거래목록', '해선      잔고평가', '해선      잔고청산', '해선      전략중지'],
-                ['코인      체결목록', '코인      거래목록', '코인      잔고평가', '코인      잔고청산', '코인      전략중지'],
+                ['주식          거래목록', '주식          잔고평가', '주식          잔고청산', '주식          전략중지'],
+                ['해선          거래목록', '해선          잔고평가', '해선          잔고청산', '해선          전략중지'],
+                ['코인          거래목록', '코인          잔고평가', '코인          잔고청산', '코인          전략중지'],
                 ['주식          라이브', '해선          라이브', '코인          라이브', '백테          라이브']
             ]
             reply_markup = ReplyKeyboardMarkup(keyboard)
+
             await self.application.bot.send_message(
                 chat_id=self.chat_id,
                 text='안녕하세요. STOM 텔레그램봇입니다.\n아래 버튼을 눌러 원하는 기능을 실행하세요.',
@@ -77,8 +89,10 @@ class TelegramBot(QThread):
                 update = await update_queue.get()
                 await self.application.process_update(update)
                 update_queue.task_done()
+
         except:
-            self.windowQ.put((ui_num['시스템로그'], f'{format_exc()}오류 알림 - 텔레그램 봇 시작'))
+            self.windowQ.put((ui_num['시스템로그'], f'{format_exc()}오류 알림 - start_bot'))
+            self.running = False
 
     async def setup_application(self, application):
         korea_timezone = pytz.timezone('Asia/Seoul')
@@ -87,11 +101,7 @@ class TelegramBot(QThread):
     # noinspection PyUnusedLocal
     async def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         cmd = update.message.text
-        if '라이브' in cmd:
-            cmd = cmd.replace('          ', '')
-        else:
-            cmd = cmd.replace('      ', '')
-
+        cmd = cmd.replace('          ', '')
         if cmd in ('주식전략중지', '해선전략중지'):
             self.wdzservQ.put(('strategy', '매수전략중지'))
         elif cmd == '코인전략중지':
@@ -160,7 +170,7 @@ class TelegramBot(QThread):
                     self.application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_message))
                     await self.start_bot()
             except:
-                self.windowQ.put((ui_num['시스템로그'], f'{format_exc()}오류 알림 - 텔레그램 봇 재시작'))
+                self.windowQ.put((ui_num['시스템로그'], f'{format_exc()}오류 알림 - restart_bot'))
                 self.running = False
 
     async def send_message(self, text):
@@ -179,3 +189,7 @@ class TelegramBot(QThread):
                 chat_id=self.chat_id,
                 photo=photo
             )
+
+    def stop(self):
+        if self.loop and self.loop.is_running():
+            self.loop.stop()
