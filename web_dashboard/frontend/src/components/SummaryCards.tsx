64b74f37d263edd1794a7756c5aaa94d624988a0
youse <youseonho@gmail.com>
2026-04-15 20:51:59 +0900

    일자 포맷 마지막 슬래시 제거
    - .replace(/\/$/, '') 추가하여 마지막 슬래시 제거
    - 04/15/ 형식에서 04/15 형식으로 변경

--- web_dashboard/frontend/src/components/SummaryCards.tsx
+++ web_dashboard/frontend/src/components/SummaryCards.tsx
@@ -31,7 +31,7 @@ export default function SummaryCards({ totalTrade, market, timestamp }: Props) {
     month: '2-digit',
     day: '2-digit',
     hour12: false
-  }).replace(/\./g, '/') : ''
+  }).replace(/\./g, '/').replace(/\/$/, '') : ''
   const formattedTime = timestamp ? new Date(timestamp).toLocaleString('ko-KR', {
     hour: '2-digit',
     minute: '2-digit',
